ae821d4e2137fdadaaa04e1506b808d7faf29c21
Said Akhrarov <akhrarovsaid30003@gmail.com>
2024-08-20 10:27:10 +0500

    Add some variables

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+
+int value = 100;
+
+Console.WriteLine(value);
+double d = 5.5;
+char c = '!';
+bool b = true;
+Console.WriteLine( d);
+Console.WriteLine(c);
+Console.WriteLine(b);
